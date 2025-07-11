9493915e34f3dc262d12a8a5fa7653b0659093bf
a-yew <jly4@sfu.ca>
2025-07-11 13:58:30 -0700

    fix: configuration for index.html, tsconfig.json and main.tsx, now app.tsx renders in localhost

---
+++ githired/src/App.tsx
@@ -0,0 +1,15 @@
+import { SearchBar } from "./components/searchbar";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="search-bar-container">
+        <div>
+          <SearchBar />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ githired/src/components/searchbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+export const SearchBar = () => {
+  return (
+    <div className="input-wrapper">
+      SearchBar <FaSearch id="search-icon" />{" "}
+    </div>
+  );
+};
