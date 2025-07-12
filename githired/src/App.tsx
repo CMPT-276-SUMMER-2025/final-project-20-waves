931b2c63f47f19e0fbc563095052cda77afa7eaf
a-yew <jly4@sfu.ca>
2025-07-11 20:20:02 -0700

    feat: added search bar with dummy API

--- githired/src/App.tsx
+++ githired/src/App.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
+
 import { SearchBar } from "./components/searchbar";
+import { SearchResultsList } from "./components/SearchResultsList";
 
 function App() {
+  const [results, setResults] = useState([]);
+
   return (
     <div className="App">
       <div className="search-bar-container">
-        <div>
-          <SearchBar />
-        </div>
+        <SearchBar setResults={setResults} />
+        {results && results.length > 0 && (
+          <SearchResultsList results={results} />
+        )}
       </div>
     </div>
   );
