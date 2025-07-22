342de574aa858f336e34d86d4a18d8ff6ef2ad29
Eric Kim <erickim@d207-023-209-087.wireless.sfu.ca>
2025-07-22 13:18:49 -0700

    alternating images home

--- githired/src/subpages/Home.tsx
+++ githired/src/subpages/Home.tsx
@@ -1,17 +1,43 @@
-import React from "react";
-
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import "../css/Home.css"
 
-import { Link } from 'react-router-dom';
+const images = [
+  '/images/Home1.png',
+  '/images/Home2.png',
+  '/images/Home3.jpg',
+];
 
 const Home: React.FC = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+        setCurrentIndex((prev) => (prev + 1) % images.length);
+    }, 5000); // Change every 5 seconds
+      return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="home-container">
-      <h1>Welcome to</h1>
-      <img src="images/Logo.png" alt="Logo" className="home-logo" />
-      <div className="home-buttons">
-        <Link to="/login" className="home-btn">Login</Link>
-        <Link to="/signup" className="home-btn">Sign Up</Link>
+    <div>
+      <div className="background-rotator">
+        {images.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            className={`bg-image ${index === currentIndex ? 'visible' : ''}`}
+            alt={`background-${index}`}
+          />
+        ))}
+      </div>
+
+      <div className="home-container">
+        <h1>Welcome to</h1>
+        <img src="/images/Logo.png" alt="Logo" className="home-logo" />
+        <div className="home-buttons">
+          <Link to="/login" className="home-btn">Login</Link>
+          <Link to="/signup" className="home-btn">Sign Up</Link>
+        </div>
       </div>
     </div>
   );
