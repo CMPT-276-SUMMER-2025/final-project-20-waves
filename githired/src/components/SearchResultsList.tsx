238eea10961db5ba115ebe40bb7eb2303934c643
a-yew <jly4@sfu.ca>
2025-07-29 04:54:01 -0700

    fix: job info will now show onclick of jobposting

--- githired/src/components/SearchResultsList.tsx
+++ githired/src/components/SearchResultsList.tsx
@@ -1,10 +1,10 @@
 import JobCard from "./JobCard";
 
-export const SearchResultsList = ({ results }) => {
+export const SearchResultsList = ({ results, onJobClick }) => {
   return (
     <div className="search-results-list">
       {results.map((job) => (
-        <JobCard key={job.id} job={job} />
+        <JobCard key={job.id} job={job} onClick={() => onJobClick(job)} />
       ))}
     </div>
   );
